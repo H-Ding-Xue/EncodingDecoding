016ee182b305f2bfcaf4a440eedecda4bce3bb03
H-Ding-Xue <95751913+H-Ding-Xue@users.noreply.github.com>
2022-10-17 20:18:21 +0800

    Delete EncodingDecoding directory

--- EncodingDecoding/src/Main.java
+++
@@ -1,8 +0,0 @@
-//private ArrayList<String> refTable = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F"));
-public class Main {
-    public static void main(String[] args) {
-        EnAndDec test = new EnAndDec();
-        System.out.println(test.encode("HELLO WORLD"));
-        System.out.println(test.decode(test.encode("HELLO WORLD")));
-    }
-}
